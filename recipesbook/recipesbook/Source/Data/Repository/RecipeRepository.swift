876c536ef6deef7acaf44ad2dc4f76ecede1490c
Pere Almendro <palmendro@bemobile.es>
2019-12-12 22:52:33 +0100

    Created RecipeRepository

---
+++ recipesbook/recipesbook/Source/Data/Repository/RecipeRepository.swift
@@ -0,0 +1,38 @@
+//
+//  RecipeRepository.swift
+//  recipesbook
+//
+//  Created by Pere Almendro on 12/12/2019.
+//  Copyright © 2019 Pere Almendro. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class RecipeRepository {
+    private let baseUrl: String = "http://www.recipepuppy.com/api/?"
+    private let sesion: URLSession = URLSession(configuration: .default)
+    
+    func fetchRecipes(query: String?, ingredients: String?, page: String?) -> Observable<Recipe> {
+        var components = URLComponents(string: baseUrl)
+        components?.queryItems = [
+            URLQueryItem(name: "i", value: ingredients),
+            URLQueryItem(name: "q", value: query),
+            URLQueryItem(name: "p", value: page)
+        ]
+        
+        guard let url = components?.url else {
+            let error = ServiceError(localizedDescription: "Invalid Request")
+            return Observable.error(error)
+        }
+        
+        let request = URLRequest(url: url)
+        return sesion.rx.response(request: request).map { (response, data) -> Recipe in
+            if 200 ..< 300 ~= response.statusCode {
+                return try JSONDecoder().decode(Recipe.self, from: data)
+            } else {
+                throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+            }
+        }
+    }
+}
--- recipesbook/recipesbook/Source/Presentation/Home/Interactor/HomeInteractor.swift
+++ recipesbook/recipesbook/Source/Presentation/Home/Interactor/HomeInteractor.swift
@@ -20,25 +20,7 @@ class HomeInteractor: HomeInteractorProtocol {
     private let sesion: URLSession = URLSession(configuration: .default)
     
     func fetchRecipes(query: String?, ingredients: String?, page: String?) -> Observable<Recipe> {
-        var components = URLComponents(string: baseUrl)
-        components?.queryItems = [
-            URLQueryItem(name: "i", value: ingredients),
-            URLQueryItem(name: "q", value: query),
-            URLQueryItem(name: "p", value: page)
-        ]
-        
-        guard let url = components?.url else {
-            let error = ServiceError(localizedDescription: "Invalid Request")
-            return Observable.error(error)
-        }
-        
-        let request = URLRequest(url: url)
-        return sesion.rx.response(request: request).map { (response, data) -> Recipe in
-            if 200 ..< 300 ~= response.statusCode {
-                return try JSONDecoder().decode(Recipe.self, from: data)
-            } else {
-                throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
-            }
-        }
+        return RecipeRepository().fetchRecipes(query: query, ingredients: ingredients, page: page)
+        // TODO : Parse Service models
     }
 }
